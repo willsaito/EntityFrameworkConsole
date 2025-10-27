712a0696c10eb1c06251967c8e36d5a35d21e4a8
William Saito <williamsaito@gmail.com>
2025-10-27 09:36:31 -0300

    second

---
+++ ConsoleApp1/Repository.cs
@@ -0,0 +1,48 @@
+﻿using ConsoleApp1.Models;
+using ConsoleApp1.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ConsoleApp1
+{
+    public class Repository
+    { 
+        private TestContext context;
+        public Repository() 
+        {
+           context = new TestContext();
+        }
+        public async Task AddAsync(Item item)
+        {
+            context.Items.Add(item);
+            await context.SaveChangesAsync();
+        }
+        public async Task DeleteByIdAsync(int id)
+        {
+            var item = GetById(id);
+            if (item is not null)
+            {
+                context.Items.Remove(item);
+                await context.SaveChangesAsync();
+
+            }
+        }
+        public Item GetById(int id)
+        {
+            var item = context.Items.FirstOrDefault(e => e.Id == id);
+            return item;
+        }
+        public List<Item> GetAll()
+        {
+            var items = context.Items.ToList();
+            return items;
+        }
+        public async Task UpdateAsync(Item item)
+        {
+            context.Entry(item).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+        }
+      
+    }
+
+}
